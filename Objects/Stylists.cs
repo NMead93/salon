9780060226beb7627506b26590057516ce3a3fa4
Nicholas Mead <nixter11@uw.edu>
2017-02-24 08:42:32 -0800

    Add stylist constructor and getall/overrides methods

---
+++ Objects/Stylists.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace Salon.Objects
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+
+    public Stylist(string name, int id = 0)
+    {
+      _id = id;
+      _name = name;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+        if (!(otherStylist is Stylist))
+        {
+          return false;
+        }
+        else
+        {
+          Stylist newStylist = (Stylist) otherStylist;
+          bool idEquality = this.GetId() == newStylist.GetId();
+          bool nameEquality = this.GetName() == newStylist.GetName();
+          return (idEquality && nameEquality);
+        }
+    }
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int StylistId = rdr.GetInt32(0);
+        string StylistName = rdr.GetString(1);
+        Stylist newStylist = new Stylist(StylistName, StylistId);
+        allStylists.Add(newStylist);
+      }
+
+      return allStylists;
+    }
+
+
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
