bf76c72b45e54e2e3899beb4df3407942cdaa26c
Nicholas Mead <nixter11@uw.edu>
2017-02-24 11:39:42 -0800

    create routes for stylist client pages and implement add client form

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,7 +20,41 @@ namespace Salon
         {
             List<Stylist> stylistList = Stylist.GetAll();
             return View["stylist-home.cshtml", stylistList];
-        }
+        };
+
+        Post["/stylists"] =_=>
+        {
+            Stylist newStylist = new Stylist(Request.Form["name"]);
+            newStylist.Save();
+            List<Stylist> stylistList = Stylist.GetAll();
+            return View["stylist-home.cshtml", stylistList];
+        };
+
+        Get["/stylists/{id}/clients"] =parameter=>
+        {
+            Stylist foundStylist = Stylist.Find(parameter.id);
+            Client id = new Client("id", parameter.id);
+            List<Client> idList = new List<Client>{id};
+            List<Client> clientList = foundStylist.GetClients();
+            Dictionary<string, List<Client>> listAndId = new Dictionary<string, List<Client>>{};
+            listAndId.Add("id", idList);
+            listAndId.Add("clients", clientList);
+            return View["clients-of-stylist.cshtml", listAndId];
+        };
+
+        Post["/stylists/{id}/clients"] =parameter=>
+        {
+            Client newClient = new Client(Request.Form["name"], parameter.id);
+            newClient.Save();
+            Stylist foundStylist = Stylist.Find(parameter.id);
+            Client id = new Client("id", parameter.id);
+            List<Client> idList = new List<Client>{id};
+            List<Client> clientList = foundStylist.GetClients();
+            Dictionary<string, List<Client>> listAndId = new Dictionary<string, List<Client>>{};
+            listAndId.Add("id", idList);
+            listAndId.Add("clients", clientList);
+            return View["clients-of-stylist.cshtml", listAndId];
+        };
 
     }
   }
